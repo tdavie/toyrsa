8ee20f354a5ac8e8a7a08b9e9ed606f81b6288c5
Tom Davie <z5263970@ad.unsw.edu.au>
2021-03-28 19:26:18 +1100

    basic argparse implementation, keygen and encrypt working

--- rsa_poc.py
+++ rsa_poc.py
@@ -2,19 +2,19 @@
 # NOT CRYPTOGRAPHICALLY SECURE
 import sys
 import argparse
+import math
 
-if __name__ == "__main__":
-    print("This is a toy implementation of RSA and not secure!")
-    parser = argparse.ArgumentParser()
-    parser.parse_args
-    # todo add args for encrypt, decrypt, keygen
-
+# read public key file
+# n, e seperated by newline
+# returns (n, e) as a tuple
+def read_pub_key(pub_key):
+    try:
+        return tuple(open(pub_key).read().splitlines())
+    except:
+        sys.exit("failed to read public key")
 
 def decrypt(ciphertext, priv_key, pub_key):
-    try:
-        pub_key_tuple = read_pub_key(pub_key)
-    except:
-        print("error reading public key")
+    pub_key_tuple = read_pub_key(pub_key)
     try:
         d = int(open(priv_key).read())
     except:
@@ -28,24 +28,22 @@ def decrypt(ciphertext, priv_key, pub_key):
 
 # very basic ecb mode implementation, encrypts each char sequentially
 def encrypt(plaintext, pub_key):
-    try:
-        pub_key_tuple = read_pub_key(pub_key)
-    except:
-        print("error reading public key")
+    #pub_key_tuple = read_pub_key(pub_key)
     ciphertext = ""
-    for char in plaintext:
-        # cipher = plain_char^(e) mod (n)
-        ciphertext += str(char^pub_key_tuple[0] % pub_key_tuple[1])
-        # add space between encrypted chars
-        ciphertext += ' '
+    for line in plaintext:
+        for char in line:
+            # cipher = plain_char^(e) mod (n)
+            ciphertext += str(ord(char)^int(pub_key[0]) % int(pub_key[1]))
+            # add space between encrypted chars
+            ciphertext += ' '
     return ciphertext
 
 # PLACEHOLDER
 def keygen():
 
     # generate two primes similar size
-    p1 = 241
-    p2 = 251
+    p1 = 61
+    p2 = 53
 
     # multiply the two to get n
     n = p1*p2
@@ -53,19 +51,18 @@ def keygen():
     # pick small odd e
     # ensure coprime with phi=(p1-1)(p2-1)
     # 65537 is the standard
-    e = 3
+    e = 17
 
     # calculate private exponent d = (2*phi(n)+1)/e
-    return (2*((p1-1)*(p2-1))+1)/e
-}
+    #return (2*((p1-1)*(p2-1))+1)/e
+
+    d = pow(e, -1, math.lcm((p1-1),(p2-1)))
+
+    print(f"Private key: {n}, {d}")
+    print(f"Public key: {n}, {e}")
 
 ## HELPER FUNCTIONS
 
-# read public key file
-# n, e seperated by newline
-# returns (n, e) as a tuple
-def read_pub_key(pub_key):
-    return tuple(open(pub_key).read().splitlines())
 
 
 # random prime generator
@@ -81,3 +78,25 @@ def read_pub_key(pub_key):
 
 
 # calculating private exponent d given phi(n) and d
+
+if __name__ == "__main__":
+    print("This is a toy implementation of RSA and not secure!")
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_text', type=str, help="Input string to be encrypted/decrypted")
+    parser.add_argument('-pub', '--pub_key', type=argparse.FileType('r'), help="Public key file")
+    parser.add_argument('-priv', '--priv_key', type=argparse.FileType('r'), help="Private key file")
+    parser.add_argument('-e', action="store_true", default=False)
+    parser.add_argument('-d', action="store_true", default=False)
+    parser.add_argument('-k', action="store_true", default=False)
+    args = parser.parse_args()
+
+
+    if args.k:
+        keygen()
+    elif args.e:
+        with args.pub_key as p_key:
+            pub_key = tuple(p_key.read().splitlines())
+            print(encrypt(args.input_text, pub_key))
+    elif (args.d):
+        print(decrypt(args.input_text, priv_key, pub_key))
